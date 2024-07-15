cda0fedf4cf45a6f248ffcb1329f473896d39517
Adarsh Kumar Debata <adarsh.debata@iserveu.co.in>
2024-07-15 12:09:25 +0530

    service added

---
+++ services/employeeService.js
@@ -0,0 +1,113 @@
+const client = require('../config/elastic');
+
+const mapResponseToSource = (response) => response.hits.hits.map(hit => hit._source);
+
+exports.findEmployeesByCriteria = async (role, department, reportingManager) => {
+  const query = {
+    bool: {
+      must: []
+    }
+  };
+
+  if (role) {
+    query.bool.must.push({ term: { role } });
+  }
+  if (department) {
+    query.bool.must.push({ term: { department } });
+  }
+  if (reportingManager) {
+    query.bool.must.push({ match: { reporting_manager: reportingManager } });
+  }
+
+  const response = await client.search({
+    index: 'employee_index',
+    body: {
+      query
+    }
+  });
+
+  return mapResponseToSource(response);
+};
+
+exports.findEmployeesByDOB = async (startDate, endDate) => {
+  const response = await client.search({
+    index: 'employee_index',
+    body: {
+      query: {
+        range: {
+          dob: {
+            gte: startDate,
+            lte: endDate
+          }
+        }
+      }
+    }
+  });
+
+  return mapResponseToSource(response);
+};
+
+exports.findEmployeesByIds = async (employeeIds) => {
+  const response = await client.mget({
+    index: 'employee_index',
+    body: {
+      ids: employeeIds
+    }
+  });
+
+  return response.docs.map(doc => doc._source);
+};
+
+exports.findEmployeesByNames = async (employeeNames) => {
+  const response = await client.search({
+    index: 'employee_index',
+    body: {
+      query: {
+        bool: {
+          should: employeeNames.map(name => ({
+            match: { employee_name: name }
+          })),
+          minimum_should_match: 1
+        }
+      }
+    }
+  });
+
+  return mapResponseToSource(response);
+};
+
+exports.findEmployeeByMobileNumber = async (mobileNumber) => {
+  const response = await client.search({
+    index: 'employee_index',
+    body: {
+      query: {
+        term: {
+          mobile_numbers: mobileNumber
+        }
+      }
+    }
+  });
+
+  return mapResponseToSource(response);
+};
+
+//method for bulk insert
+exports.bulkInsertEmployees = async (employees) => {
+  const bulkBody = [];
+
+  employees.forEach(emp => {
+    bulkBody.push({ index: { _index: 'employee_index', _id: emp.id } });
+    bulkBody.push(emp);
+  });
+
+  const response = await client.bulk({
+    refresh: true,
+    body: bulkBody
+  });
+
+  if (response.errors) {
+    throw new Error('Error in bulk insertion');
+  }
+
+  return response;
+};
