3c7c6b8d3b6b4ae49b4bb5373a0773d4b5f05ff0
Adarsh Kumar Debata <adarsh.debata@iserveu.co.in>
2024-07-15 15:52:14 +0530

    Server modified for swagger

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
 const employeeRoutes = require('./routes/employeeRoutes');
 const logger = require('./middlewares/logger');
 const logRoutes = require('./middlewares/logRoutes');
@@ -6,15 +8,20 @@ const logRoutes = require('./middlewares/logRoutes');
 const app = express();
 
 app.use(logger);
-
 app.use(express.json());
 app.use('/data', employeeRoutes);
 
 // Log all API endpoints and methods on server start
 logRoutes(app);
 
-const PORT = process.env.PORT || 3000;
+// Serve Swagger JSON
+app.use('/swagger-output.json', express.static(path.join(__dirname, 'swagger-output.json')));
 
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(require('./swagger-output.json')));
+
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
