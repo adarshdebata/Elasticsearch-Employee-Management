8b057d8b9898e0a15340dd1e3fc0762f06ea68b8
Adarsh Kumar Debata <adarsh.debata@iserveu.co.in>
2024-07-15 12:07:34 +0530

    Log for request

---
+++ middlewares/logger.js
@@ -0,0 +1,19 @@
+const logger = (req, res, next) => {
+    const startTime = process.hrtime();
+    const { method, url } = req;
+    
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const duration = process.hrtime(startTime);
+      const durationInMs = duration[0] * 1000 + duration[1] / 1e6;
+      const contentLength = res.getHeader('content-length');
+      const logMessage = `My Logger : ${method} ${url} ${statusCode} ${durationInMs.toFixed(2)}ms ${contentLength}bytes\n`;
+  
+      console.log(logMessage); 
+
+    });
+  
+    next();
+  };
+  module.exports = logger;
+  
