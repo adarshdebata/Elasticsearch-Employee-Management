6500916e4345fa08a3e499f596016ba33e19638c
Adarsh Kumar Debata <adarsh.debata@iserveu.co.in>
2024-07-16 12:49:41 +0530

    swagger.js added for API documentation

---
+++ swagger.js
@@ -0,0 +1,30 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const packageInfo = require('./package.json');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: packageInfo.name,
+      version: packageInfo.version,
+      description: packageInfo.description,
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000', // Change this to your server URL
+      },
+    ],
+  },
+  apis: ['./routes/*.js', './controllers/*.js'], // Paths to files containing OpenAPI definitions
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get('/api-docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+};
